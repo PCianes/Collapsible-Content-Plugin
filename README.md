6b86cc863217da488f1fe2117132965716188aa3
PCianes <cianespablo@gmail.com>
2017-07-12 11:33:01 +0200

    Init

---
+++ README.md
@@ -0,0 +1,31 @@
+# Collapsible content plugin
+
+Collapsible Content is a WordPress Plugin that shows and hides content.
+Click the icon to open and reveal the content.
+Click again to close and hide it.
+
+## Features
+
+This plugin includes the following features:
+
+- QA Shortcode, `[qa]`
+- Teaser Shortcode, `[teaser]`
+- Font icon visual indicator
+- jQuery sliding animation
+
+## Installation
+
+To install this plugin, yo can download it by clicking on the GitHub download button or clone the repository.
+
+- Navigate to the `wp-content/plugins` folder of your project
+- Clone the repository by copying/pasting this line of code in terminal:
+ `git clone git@github.com:PCianes/Collapsible-content-plugin.git`.
+- Log into your WordPress website and go to plugins to activate the plugin Collapsible content plugin.
+
+## Continue Development
+
+If you want to continue development, you will need to have Composer, Gulp, Node.js and npm installed on your machine. 
+
+- Navigate to the `wp-content/plugins/collapsible-content` folder. 
+- Type `npm install` to install all of the `node_modules` for Gulp.
+- Type `composer install` to install the Composer PHP packages.
---
+++ bootstrap.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Collapsible Content plugin
+ *
+ * @package     	PCianes\CollapsibleContent
+ * @author      	Pablo Cianes
+ * @license     	GPL-2.0+
+ * @link 			https://pablocianes.com
+ *
+ * @wordpress-plugin
+ * Plugin Name: 	Collapsible content plugin
+ * Plugin URI:  	-
+ * Description: 	Collapsible Content is a WordPress Plugin that shows and hides content.
+ * Version:     	1.0.0
+ * Author:      	Pablo Cianes
+ * Author URI:  	https://pablocianes.com
+ * Text Domain: 	collapsible_content
+ * Requires WP:		4.8
+ * Requires PHP:	5.5
+ * License:     	GPL-2.0+
+ * License URI: 	http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * Collapsible content plugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ * 
+ * Collapsible content plugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Collapsible content plugin. If not, see http://www.gnu.org/licenses/gpl-2.0.txt.
+ *
+ */
+
+namespace PCianes\CollapsibleContent;
+
+if ( !defined( 'ABSPATH' ) ) {
+	exit( 'Cheatin&#8217; uh?' );
+}
+
+require_once( __DIR__ . '/assets/vendor/autoload.php');
+
+
+add_action( 'init', __NAMESPACE__ . '\plugin_launch');
+
+function plugin_launch(){
+
+}
